79461bae50f593f92d92e0c4032874ebe479f838
MMar1ey13 <mwendamarley12@gmail.com>
2023-06-14 02:36:05 -0700

    Task1

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    """
+    Computes the square of all integers of a matrix
+    ...
+
+    Parameters
+    -----
+    matrix : list (of lists)
+        The list of elements
+
+    Return:
+        a new matrix
+        Same size as matrix
+        Each value should be the square of the value of the input
+    """
+
+    return ([list(map(lambda x: x * x, row)) for row in matrix])
+
+
