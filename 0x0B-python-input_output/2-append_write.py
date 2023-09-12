652de6af9f56b103a0cc21e506a6d8c312751ee0
MMarley <mwendamarley12@gmail.com>
2023-09-12 13:21:02 -0700

    first

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -4,9 +4,7 @@
 
 def append_write(filename="", text=""):
     """ appends string and returns number of characters """
-    
     string = 0
     with open(filename, "a", encoding="utf-8") as f:
         string = f.write(text)
-
     return string
