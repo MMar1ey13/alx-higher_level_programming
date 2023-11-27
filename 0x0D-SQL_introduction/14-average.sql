2db28c0dcc73412235fe197a6724d05c087c6d95
MARLEY <127131158+MMar1ey13@users.noreply.github.com>
2023-11-27 23:28:05 +0300

    Delete 0x0D-SQL_introduction directory

--- 0x0D-SQL_introduction/14-average.sql
+++
@@ -1,2 +0,0 @@
--- script that computes the score average of all records in the table 
-SELECT AVG(score) as average FROM  `second_table`;
--- 0x0D-SQL_introduction/4-first_table.sql
+++
@@ -1,5 +0,0 @@
--- creates a table called first_table in the current database
-CREATE TABLE IF NOT EXISTS `first_table` (
-		`id` int NOT NULL,
-			`name` varchar(256)
-			);
